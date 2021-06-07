896d34dc01e34bb7e347f0ff52d159988b54600e
edg_alves <edgar.costa@dunelm.com>
2021-06-07 13:13:34 +0100

    Aula 4

--- src/components/btn/index.jsx
+++ src/components/btn/index.jsx
@@ -6,7 +6,7 @@ const Button = (props) => {
     const {text,chevron,disabled,onClickCallback}=props;
 
     return (
-        <button className={disabled?"disabled":""}  onClick={() => onClickCallback()}>
+        <button className={disabled?"disabled":""} disabled={disabled} onClick={() => onClickCallback()}>
             <div className={chevron && chevron=="left"?"chevron-reverse btn-container chevron " + chevron:"chevron btn-container chevron " + chevron}>
                 <label className="btn" >{text}</label>
             </div>
