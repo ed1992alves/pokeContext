ca332996fd676ae96eef9ee7dc4867a7259a1184
Tójó <antonio.cruzgei@gmail.com>
2021-06-08 11:02:26 +0100

    product page fix

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -34,43 +34,45 @@ const Product = () => {
         </Link>
         {!product && <div className="loading">Loading...</div>}
 
-        {product && (
-          <div className="product">
-            <img
-              className="img"
-              src={product.image_front_small_url}
-              alt="Product Image"
-            ></img>
-            <div className="product-description">
-              <p>{product.product_name}</p>
-              <p>
-                <b>Brand:</b> {product?.brands}
-              </p>
-              <p>
-                <b>Quantity:</b> {product?.product_quantity}g
-              </p>
-              <p>
-                <b>Carbohydrates per 100g:</b>{" "}
-                {product?.nutriments.carbohydrates}g
-              </p>
-              <p>
-                <b>Energy:</b> {product?.nutriments.energy}kcal
-              </p>
-              <p>
-                <b>Fat:</b> {product?.nutriments.fat}g
-              </p>
-              <p>
-                <b>Fiber:</b> {product?.nutriments.fiber}g
-              </p>
-              <p>
-                <b>Proteins:</b> {product?.nutriments.proteins}g
-              </p>
-              <p>
-                <b>Sugars:</b> {product?.nutriments.sugars}g
-              </p>
+        <div className="product-info">
+          {product && (
+            <div className="product">
+              <img
+                className="img"
+                src={product.image_front_small_url}
+                alt="Product Image"
+              ></img>
+              <div className="product-description">
+                <p>{product.product_name}</p>
+                <p>
+                  <b>Brand:</b> {product?.brands}
+                </p>
+                <p>
+                  <b>Quantity:</b> {product?.product_quantity}g
+                </p>
+                <p>
+                  <b>Carbohydrates per 100g:</b>{" "}
+                  {product?.nutriments.carbohydrates}g
+                </p>
+                <p>
+                  <b>Energy:</b> {product?.nutriments.energy}kcal
+                </p>
+                <p>
+                  <b>Fat:</b> {product?.nutriments.fat}g
+                </p>
+                <p>
+                  <b>Fiber:</b> {product?.nutriments.fiber}g
+                </p>
+                <p>
+                  <b>Proteins:</b> {product?.nutriments.proteins}g
+                </p>
+                <p>
+                  <b>Sugars:</b> {product?.nutriments.sugars}g
+                </p>
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </>
   );
